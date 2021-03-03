48d155b42d07b98302f0cf0dfa52c52998961f43
tuananhpta19 <tuananh.pta19@gmail.com>
2021-03-03 20:17:28 +0700

    jwt & cookie

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,12 @@ var path = require("path")
 router.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "../views/home.html"))
 })
-
+router.get("/login", function(req,res){
+    res.sendFile(path.join(__dirname, "../views/login.html"))
+})
+router.get("/demo", function(req,res){
+    res.sendFile(path.join(__dirname, "../views/demo.html"))
+})
 router.get("/detail-user/:id", function(req,res){
     res.sendFile(path.join(__dirname, "../views/detailUser.html"))
 })
--- routers/user.js
+++ routers/user.js
@@ -4,6 +4,27 @@ var userServies = require("../services/userServies");
 const bcrypt = require('bcrypt');
 const { updateUser } = require("../services/userServies");
 const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+
+//api giải mã token
+router.get("/decode", function(req, res){
+    try {
+          // body, params, query
+      var token = req.query.token || req.headers.authorization.split("Bearer ")[1];
+      var decode = jwt.verify(token, "nodemy");
+      userServies.getDetailUser(decode._id)
+      .then((result) => {
+        res.json({
+            message: "thông tin người dùng",
+            user: result
+        })
+      })
+    } catch (error) {
+        res.json(error)
+    }
+  })
+  
+
 
 router.get("/", function(req, res){
     //hiển thị toàn dữ liệu trong database
@@ -96,15 +117,21 @@ router.post("/login", function(req, res){
         }
         bcrypt.compare(password, user.password).then(function(result) {
             if(result){
+                var token = jwt.sign({ _id: user._id }, "nodemy", { expiresIn: "1d"});
+                // giá trị mà mình sẽ sử dụng sau khi người dùng gửi lại mã token này
+                // privateKey: là thông tin khóa bí mật(khóa này không được tiết lộ ra ngoài)
+                // option: thông tin thuật toán mã hóa, ... thời gian tồn tại của token: expiresIn dưới dạng ms
                 return res.json({
                     message: "Đăng nhập thành công",
                     error: false,
-                    user: user
+                    user: user,
+                    token: token
                 })
             }
+         
             return res.json({
                 message: "Đăng nhập không thành công",
-                error: true
+                error: true,
             })
         });
     }).catch((err) => {
@@ -114,5 +141,7 @@ router.post("/login", function(req, res){
         })
     });
 })
+
+
 
 module.exports = router
